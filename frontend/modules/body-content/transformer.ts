fef62d52522b0d8598ed81b03fc2d895dae8e0f8
Nicolas Thumann <me@n-thumann.de>
2023-12-10 01:28:21 +0100

    Fix: File title casing

--- frontend/modules/body-content/transformer.ts
+++ frontend/modules/body-content/transformer.ts
@@ -5,9 +5,11 @@ export default defineTransformer({
   name: "body-transformer",
   extensions: [".json"],
   parse: (_id: string, content: any) => {
+    const path = _id.split(":");
     return {
+      title: path[path.length - 1].slice(0, -5),
       body: content,
-      path: _id.split(":"),
+      path,
       _id,
     };
   },
