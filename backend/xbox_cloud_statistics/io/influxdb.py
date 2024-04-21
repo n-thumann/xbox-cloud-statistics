3e56dc8693b0f637b7999ed68aa5151c40a1c3a6
Nicolas Thumann <me@n-thumann.de>
2024-04-21 23:48:46 +0200

    Add(backend): InfluxDB handler

---
+++ backend/xbox_cloud_statistics/io/influxdb.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from pathlib import Path
+
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+from xbox_cloud_statistics.models import Measurement, Results
+
+from .io import IO
+
+class InfluxDB(IO):
+    @staticmethod
+    def handle(url: str, token: str, org: str, bucket: str, results: Results):
+
+        points = []
+        for game, regions in results:
+            for region, subscriptions in regions:
+                for subscription, measurement in subscriptions:
+                    measurement
+                    point = (
+                        Point("measurement")
+                        .tag("game", game.id)
+                        .tag("region", region.name)
+                        .tag("subscription", subscription)
+                        .time(measurement.server_time)
+                        .field("wait_time", measurement.wait_time)
+                    )
+                    print(point)
+
+                    points.append(point)
+
+        with InfluxDBClient(url=url, token=token, org=org) as client:
+            write_api = client.write_api(write_options=SYNCHRONOUS)
+            write_api.write(bucket=bucket, org=org, record=points)
