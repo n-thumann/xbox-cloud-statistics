5cb1f692ebde73c5171c52c33664d94efe47a82e
Nicolas Thumann <me@n-thumann.de>
2024-05-05 00:51:55 +0200

    Remove(frontend): Unused dependencies

--- site_generator/main.py
+++ site_generator/main.py
@@ -1,7 +1,5 @@
 from collections import defaultdict
-from datetime import datetime, timedelta, timezone
-import json
-from pathlib import Path
+from datetime import datetime, timedelta
 
 from common.models import Game
 from site_generator.config import FrontendConfig
