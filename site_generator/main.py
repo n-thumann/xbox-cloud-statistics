0d768765ad87d006c230fbf753417aeaba536f3a
Nicolas Thumann <me@n-thumann.de>
2024-05-17 19:42:51 +0200

    Change: Rename variables

--- site_generator/main.py
+++ site_generator/main.py
@@ -13,12 +13,12 @@ from influxdb_client import InfluxDBClient
 def main():
     config = FrontendConfig()
     templates = Templates()
-    games_meta = config.games
+    games = config.games
 
     with InfluxDBClient(
         url=config.influxdb_url, token=config.influxdb_token, org=config.influxdb_org
     ) as client:
-        games = get_games(client, config.influxdb_bucket)
+        measurements = get_measurements(client, config.influxdb_bucket)
         last_update = get_last_update(client, config.influxdb_bucket)
 
     defaults = {
@@ -27,26 +27,28 @@ def main():
         "last_update": last_update,
     }
 
-    write_index(templates, defaults, games_meta)
-    write_games(templates, defaults, games_meta, games)
+    write_index(templates, defaults, games)
+    write_games(templates, defaults, games, measurements)
 
 
-def write_index(template: Templates, defaults: dict, games_meta: list):
-    template.render("index", "index", **defaults, games=games_meta)
+def write_index(template: Templates, defaults: dict, games: list):
+    template.render("index", "index", **defaults, games=games)
 
 
 def write_games(
-    template: Templates, defaults: dict, games_meta: list[Game], games: dict
+    template: Templates, defaults: dict, games: list[Game], measurements: dict
 ):
-    for game in games_meta:
-        results = games.get(game.id)
-
+    for game in games:
         template.render(
-            "game", f"games/{game.id}", **defaults, title=game.title, results=results
+            "game",
+            f"games/{game.id}",
+            **defaults,
+            title=game.title,
+            measurements=measurements.get(game.id),
         )
 
 
-def get_games(influxdb_client: InfluxDBClient, influxdb_bucket: str):
+def get_measurements(influxdb_client: InfluxDBClient, influxdb_bucket: str):
     query_api = influxdb_client.query_api()
     tables = query_api.query(f'from(bucket:"{influxdb_bucket}") |> range(start: -3d)')
 
